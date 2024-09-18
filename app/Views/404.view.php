e5ef580a76c5e6d1577cabeedccfd60a13198dc2
Abdou-khadre78 <dabdou2003@gmail.com>
2024-09-18 12:12:30 +0200

    mise en place de la page redirection

---
+++ app/Views/404.view.php
@@ -0,0 +1,5 @@
+<?php
+    require_once(__DIR__ . "/partials/head.php");
+?>
+<a class="btn btn-primary" href="404.view.php" role="button">Link</a>
+<button class="btn btn-primary" type="submit">Retour accueil</button>
